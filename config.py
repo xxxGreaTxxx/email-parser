3fa13b94d425aedff4fceb30992eb7a6808ac6a3
Nick Barinov <great1989@mail.ru>
2023-07-13 19:06:21 +0300

    добавлено описание

---
+++ config.py
@@ -0,0 +1,27 @@
+config_vars = {
+    'email_server': 'imap.mail.ru',
+    'folder': 'INBOX',
+    'from': 'notifications@mail.bothelp.io',
+    'sites': {
+        'nachtrad': {
+            'bots': [
+                'Cskills',
+            ],
+            'url': 'https://makeevedu.ru/',
+            'form': {
+                'form': 't-form__inputsbox',
+                'name': '[name="Name"]',
+                'phone': '[type="tel"]'
+            },
+        },
+        'seven_shares': {
+            'bots': [],
+            'url': 'seven_shares',
+            'form': {
+                'form': 't-form__inputsbox',
+                'name': '[name="Name"]',
+                'phone': '[type="tel"]'
+            },
+        }
+    },
+}
